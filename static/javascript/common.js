bb9c619ae7894681aaa02f0727296099901a28c5
zoan57 <105167620+zoan57@users.noreply.github.com>
2022-12-26 05:28:57 +0800

    modify the close button

--- static/javascript/common.js
+++ static/javascript/common.js
@@ -18,7 +18,7 @@ class Header extends HTMLElement {
             <div id="loginCard" class="loginFrame">
                 <form class="userInfo">
                     <h3>登入會員帳號</h3>
-                    <span id="btnTurnOff"></span>
+                    <span id="loginTurnOff"></span>
                     <input type="email" name="loginEmail" placeholder="輸入電子信箱" class="userInfoCheck" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="請輸入有效的信箱">
                     <input type="password" name="loginPwd" autocomplete="off" placeholder="輸入密碼" class="userInfoCheck" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="密碼必須包含至少一個數字(0-9) 、一個大寫英文字母(A-Z)、一個小寫英文字母(a-z)，長度應至少8碼以上。">
                     <button type="button" id="loginBtn">登入帳戶</button>
@@ -31,7 +31,7 @@ class Header extends HTMLElement {
             <div id="registerCard" class="registerFrame">
                 <form class="userInfo">
                     <h3>註冊會員帳號</h3>
-                    <span id="btnTurnOff"></span>
+                    <span id="registerTurnOff"></span>
                     <input type="text" name="name" placeholder="輸入姓名" maxlength="20" class="userInfoCheck" required>
                     <input type="email" name="registerEmail" placeholder="輸入電子信箱" class="userInfoCheck" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="請輸入有效的信箱">
                     <input type="password" name="registerPwd" autocomplete="off" placeholder="輸入密碼" class="userInfoCheck" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="密碼須包含大小寫英文字母，並需超過8個字元">
@@ -75,7 +75,11 @@ register.addEventListener("click", (event) => {
         register.style.display = "none";
     }
 })
-btnTurnOff.addEventListener("click", (event) => {
+loginTurnOff.addEventListener("click", (event) => {
+    register.style.display = "none";
+    login.style.display = "none";
+})
+registerTurnOff.addEventListener("click", (event) => {
     register.style.display = "none";
     login.style.display = "none";
 })
